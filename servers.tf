4a5cd8e710805d371cb4ce99355a374bece5d884
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-01 04:42:56 +0530

    correction

--- servers.tf
+++ servers.tf
@@ -89,22 +89,6 @@ resource "aws_route53_record" "user" {
   records = [aws_instance.user.private_ip]
 }
 
-resource "aws_instance" "cart" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_route53_record" "cart" {
-  zone_id = Z01821482DK31A4C4NHX5
-  name    = "cart-dev.devjsr1.online"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.cart.private_ip]
-}
 
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
@@ -174,6 +158,23 @@ resource "aws_route53_record" "payment" {
   records = [aws_instance.payment.private_ip]
 }
 
+
+resource "aws_instance" "dispatch" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "dispatch"
+  }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = Z01821482DK31A4C4NHX5
+  name    = "dispatch-dev.devjsr1.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.dispatch.private_ip]
+}
 
 
 
