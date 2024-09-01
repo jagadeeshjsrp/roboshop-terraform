a6e436aa62d963fd4efec6a114fc86d9c69815c5
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-02 01:39:20 +0530

    correction

--- servers.tf
+++ servers.tf
@@ -17,10 +17,10 @@ variable "components" {
 }
 
 resource "aws_instance" "instance" {
-  count = length(var.components)
+  count         = length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = ["data.aws_security_group.allow-all.id"]
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = var.components[count.index]
