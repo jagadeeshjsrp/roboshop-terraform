fe406be5cd8a7517d8f8216ab08ccdbfce691301
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-03 15:31:34 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -7,13 +7,19 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+}
 
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  for_each = var.components
   provisioner "remote-exec" {
+
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = self.private_ip
+      host     = aws_instance.instance[each.value["name"]].private_ip
     }
 
     inline = [
@@ -24,7 +30,6 @@ resource "aws_instance" "instance" {
     ]
   }
 }
-
 resource "aws_route53_record" "records" {
   for_each   = var.components
   zone_id = "Z01821482DK31A4C4NHX5"
