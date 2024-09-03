731e28952cdd94f808559435e14f98b7d7494bf6
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-03 21:54:19 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,45 +1,12 @@
-resource "aws_instance" "instance" {
+module "servers" {
   for_each = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
-  tags = {
-    Name = each.value["name"]
-  }
+  source = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
 }
-
-resource "null_resource" "provisioner" {
-
-  depends_on = [aws_instance.instance, aws_route53_record.records]
-  for_each = var.components
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance[each.value["name"]].private_ip
-    }
-
-    inline = [
-      "rm -rf roboshop",
-      "git clone https://github.com/jagadeeshjsrp/roboshop",
-      "cd roboshop",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null") }"
-    ]
-  }
-}
-
-resource "aws_route53_record" "records" {
-  for_each   = var.components
-  zone_id = "Z01821482DK31A4C4NHX5"
-  name    = "${each.value["name"]}-dev.devjsr1.online"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.instance[each.value["name"]].private_ip]
-}
-
 /*resource "aws_route53_record" "frontend" {
   zone_id = "Z01821482DK31A4C4NHX5"
   name    = "frontend-dev.devjsr1.online"
