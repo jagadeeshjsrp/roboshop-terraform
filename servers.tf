07de4884e497e85f08751ee993b4c06a7203d016
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-05 11:06:46 +0530

    back terraform

--- servers.tf
+++ servers.tf
@@ -54,11 +54,11 @@ variable "components" {
   }
 }
 
-resource "aws_instance" "frontend" {
-  for.each      = var.components
+resource "aws_instance" "instance" {
+  for_each      = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = ["data.aws_security_group.allow-all.id"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = each.value["name"]
