00222a0c34c45f8bac5d4139c8d261d6a3823aee
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-11 08:27:45 +0530

    Ansible roboshop

--- module/locals.tf
+++ module/locals.tf
@@ -1,3 +1,13 @@
 locals {
   name =  var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
+  db_commands = [
+    "rm -rf roboshop",
+    "git clone https://github.com/jagadeeshjsrp/roboshop",
+    "cd roboshop",
+    "sudo bash ${var.component_name}.sh ${var.password}"
+  ]
+  app_commands = [
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/jagadeeshjsrp/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+  ]
 }
