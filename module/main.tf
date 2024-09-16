8ee7ad2cba5ef78cd587334a21abe6295f48be64
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-16 16:01:28 +0530

    Ansible roboshop

--- module/main.tf
+++ module/main.tf
@@ -76,18 +76,16 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Sid": "VisualEditor0",
         "Effect": "Allow",
         "Action": [
+          "kms:Encrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:730335618466:parameter/${var.env}.${var.component_name}.*"
-      },
-      {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:DescribeParameters",
-        "Resource": "*"
+        "Resource": [
+          "arn:aws:kms:us-east-1:730335618466:key/cda6780d-7d23-4924-8c45-67bc70f59601",
+          "arn:aws:ssm:us-east-1:730335618466:parameter/${var.env}.${var.component_name}.*"
+        ]
       }
     ]
   })
